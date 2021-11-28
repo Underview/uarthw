e8560e349a60e39849f512c651e932b846bc7173
ABDALLAH EL MOUAATAMID <abdaelmouaatamid@gmail.com>
2021-11-28 13:44:04 +0100

    project3

--- uarthw/hello_world.c
+++ uarthw/hello_world.c
@@ -19,6 +19,19 @@
 #include "sys/alt_timestamp.h"
 #include "altera_avalon_pio_regs.h"
 #include "sys/alt_irq.h"
+/*define as global variable*/
+  int *uart_ba = (int *) UART_0_BASE; /*0x8001060 - 0x0800107f*/
+  int rxdataoffs = 0;
+  int txdataoffs = 1;
+  int statusoffs = 2;
+  int ctrloffs = 3;
+  int divisoroffs = 4;
+
+
+
+
+
+
 #define PROJECT1
 
 
@@ -26,12 +39,7 @@
 
 int main()
 {
-  int *uart_ba = (int *) UART_0_BASE;
-  int rxdataoffs = 0;
-  int txdataoffs = 1;
-  int statusoffs = 2;
-  int ctrloffs = 3;
-  int divisoroffs = 4;
+
 
   printf("Base address: 0x%08x (%d)\n", (int) uart_ba,(int) uart_ba);
   printf("RXDATA address = 0x%08x (%d) - ", (int) (uart_ba+rxdataoffs), (int)(uart_ba+rxdataoffs));
@@ -52,3 +60,88 @@ int main()
 }
 
 #endif
+
+
+#ifdef PROJECT2
+int main()
+{
+	//with a baudrate of 115200 we obtain 433
+	//with a baudrate of 2400 we obtain 20832(0x00005160)
+	*( uart_ba + divisoroffs) = 0x00005160;
+	 printf("DIVISOR address = 0x%08x (%d) - ", (int) (uart_ba+divisoroffs), (int)(uart_ba+divisoroffs));
+	 printf("value = %d\n", *(uart_ba + divisoroffs));
+
+
+
+
+}
+#endif
+
+
+#ifdef PROJECT31
+int main()
+{
+	char str ='c'; //modify this parameter to send one-two-three characters
+	int i;
+	//with a baudrate of 115200 we obtain 433
+	//with a baudrate of 2400 we obtain 20832(0x00005160)
+	*( uart_ba + divisoroffs) = 0x00005160;
+	 printf("DIVISOR address = 0x%08x (%d) - ", (int) (uart_ba+divisoroffs), (int)(uart_ba+divisoroffs));
+	 printf("value = %d\n", *(uart_ba + divisoroffs));
+
+
+		 printf("value = %d\n", *(uart_ba + statusoffs));//before transmission
+		 *( uart_ba + txdataoffs ) = (int)str; //writing the register
+		 printf("value = %d\n", *(uart_ba + statusoffs));
+
+
+
+
+}
+#endif
+
+#ifdef PROJECT32
+int main()
+{
+	char str[i] ="ci"; //modify this parameter to send one-two-three characters
+	int i;
+	//with a baudrate of 115200 we obtain 433
+	//with a baudrate of 2400 we obtain 20832(0x00005160)
+	*( uart_ba + divisoroffs) = 0x00005160;
+	 printf("DIVISOR address = 0x%08x (%d) - ", (int) (uart_ba+divisoroffs), (int)(uart_ba+divisoroffs));
+	 printf("value = %d\n", *(uart_ba + divisoroffs));
+
+
+		 printf("value = %d\n", *(uart_ba + statusoffs));//before transmission
+		 *( uart_ba + txdataoffs ) = (int)str[0]; //writing the register
+		 *( uart_ba + txdataoffs ) = (int)str[1];
+		 printf("value = %d\n", *(uart_ba + statusoffs));
+
+
+
+
+}
+#endif
+
+#ifdef PROJECT32
+int main()
+{
+	char str[i] ="cia"; //modify this parameter to send one-two-three characters
+	int i;
+	//with a baudrate of 115200 we obtain 433
+	//with a baudrate of 2400 we obtain 20832(0x00005160)
+	*( uart_ba + divisoroffs) = 0x00005160;
+	 printf("DIVISOR address = 0x%08x (%d) - ", (int) (uart_ba+divisoroffs), (int)(uart_ba+divisoroffs));
+	 printf("value = %d\n", *(uart_ba + divisoroffs));
+
+
+		 printf("value = %d\n", *(uart_ba + statusoffs));//before transmission
+		 *( uart_ba + txdataoffs ) = (int)str[0]; //writing the register
+		 *( uart_ba + txdataoffs ) = (int)str[1];
+		 *( uart_ba + txdataoffs ) = (int)str[2];
+		 printf("value = %d\n", *(uart_ba + statusoffs));
+
+
+
+
+}
--- uarthw/obj/default/hello_world.d
+++ uarthw/obj/default/hello_world.d
@@ -1,1 +1,39 @@
-obj/default/hello_world.o: hello_world.c
+obj/default/hello_world.o: hello_world.c ../uarthw_bsp/system.h \
+ ../uarthw_bsp/linker.h ../uarthw_bsp//HAL/inc/sys/alt_timestamp.h \
+ ../uarthw_bsp//HAL/inc/alt_types.h \
+ ../uarthw_bsp//drivers/inc/altera_avalon_timer.h \
+ ../uarthw_bsp//HAL/inc/sys/alt_dev.h \
+ ../uarthw_bsp//HAL/inc/sys/alt_llist.h \
+ ../uarthw_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../uarthw_bsp//HAL/inc/sys/alt_warning.h \
+ ../uarthw_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../uarthw_bsp//HAL/inc/io.h ../uarthw_bsp//HAL/inc/alt_types.h \
+ ../uarthw_bsp//HAL/inc/sys/alt_irq.h ../uarthw_bsp//HAL/inc/nios2.h
+
+../uarthw_bsp/system.h:
+
+../uarthw_bsp/linker.h:
+
+../uarthw_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../uarthw_bsp//HAL/inc/alt_types.h:
+
+../uarthw_bsp//drivers/inc/altera_avalon_timer.h:
+
+../uarthw_bsp//HAL/inc/sys/alt_dev.h:
+
+../uarthw_bsp//HAL/inc/sys/alt_llist.h:
+
+../uarthw_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../uarthw_bsp//HAL/inc/sys/alt_warning.h:
+
+../uarthw_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../uarthw_bsp//HAL/inc/io.h:
+
+../uarthw_bsp//HAL/inc/alt_types.h:
+
+../uarthw_bsp//HAL/inc/sys/alt_irq.h:
+
+../uarthw_bsp//HAL/inc/nios2.h:
